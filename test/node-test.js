0445ee5f07c6b31308073d97871e2bb08dc848a2
Rode <r.madiande@gmail.com>
2019-08-12 21:54:42 +0200

    feat/test: some adjusting on Node lib.

--- test/node-test.js
+++ test/node-test.js
@@ -5,26 +5,66 @@ const expect = chai.expect
 const Node = require('../lib/node.js')
 
 describe('Node', () => {
-  it('add(12.5, charly) should NOT insert a node in the binary search tree because, the key must be an integer.', (done) => {
+  it('constructor(12.5) should NOT create a node passing a float because, the key must be an integer.', (done) => {
     try {
-      const node = new Node(12.5, 'charly')
+      const node = new Node(12.5)
     } catch(err) {
       expect(err.message).to.equal('must be an integer!')
     }
     done()
   })
-  it('add(667, ) should NOT insert a node in the binary search tree because, the bidderName cannot be null..', (done) => {
-    try{
-      const node = new Node(667, '')
-    }catch(err) {
-      expect(err.message).to.equal('bidderName cannot be null!')
+  it('constructor(true) should NOT create a node passing a boolean  because, the key must be an integer.', (done) => {
+    try {
+      const node = new Node('coucou')
+    } catch(err) {
+      expect(err.message).to.equal('must be an integer!')
     }
     done()
   })
-  it('add(957, charly) should insert a node in the binary search tree having a key equal to 957, and the bidders property should have charly inserted in the array.', (done) => {
-    const node = new Node(957, 'charly')
-    expect(node.key).to.equal(957)
-    expect(node.bidders).to.eql(['charly'])
+  it('constructor(undefined) should NOT create a node passing a string  because, the key must be an integer.', (done) => {
+    try {
+      const node = new Node(undefined)
+    } catch(err) {
+      expect(err.message).to.equal('must be an integer!')
+    }
+    done()
+  })
+  it('constructor({}) should NOT create a node passing an object  because, the key must be an integer.', (done) => {
+    try {
+      const node = new Node('coucou')
+    } catch(err) {
+      expect(err.message).to.equal('must be an integer!')
+    }
+    done()
+  })
+  it('constructor(null) should NOT create a node passing a null  because, the key must be an integer.', (done) => {
+    try {
+      const node = new Node(null)
+    } catch(err) {
+      expect(err.message).to.equal('must be an integer!')
+    }
+    done()
+  })
+  it('constructor(`12`) should NOT create a node passing a string  because, the key must be an integer.', (done) => {
+    try {
+      const node = new Node('12')
+    } catch(err) {
+      expect(err.message).to.equal('must be an integer!')
+    }
+    done()
+  })
+  it('constructor(667) should create a node with a bidders property having *reservePrice(empty) and a key equal to 667.', (done) => {
+    const node = new Node(667)
+    expect(node.key).to.equal(667)
+    expect(node.bidders).to.eql(['*reservePrice'])
+    expect(node.left).to.equal(null)
+    expect(node.right).to.equal(null)
+    done()
+  })
+  it('constructor(667, marcus) should create a node with a bidders property having marcus and a key equal to 667.', (done) => {
+    const node = new Node(667, 'marcus')
+    expect(node.key).to.equal(667)
+    expect(node.bidders).to.eql(['marcus'])
     expect(node.left).to.equal(null)
     expect(node.right).to.equal(null)
     done()
