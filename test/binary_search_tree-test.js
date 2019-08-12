84f73c4b1b59a6ee820462d28e548490cff63866
Rode <r.madiande@gmail.com>
2019-08-12 23:25:56 +0200

    style: formatting no code change

--- test/binary_search_tree-test.js
+++ test/binary_search_tree-test.js
@@ -1,11 +1,11 @@
-`use strict`
+;`use strict`
 
 const chai = require('chai')
 const expect = chai.expect
 const BST = require('../lib/binary_search_tree.js')
 
 describe('Binary search tree', () => {
-  it('add(119) should insert a node root in the binary search tree having a key equal to 119', (done) => {
+  it('add(119) should insert a node root in the binary search tree having a key equal to 119', done => {
     const bst = new BST()
     bst.add(119)
     expect(bst.root.key).to.be.equal(119)
@@ -14,17 +14,17 @@ describe('Binary search tree', () => {
     expect(bst.root.bidders).to.be.eql(['*reservePrice'])
     done()
   })
-  it('add(567) should NOT insert a second node without bidderName', (done) => {
+  it('add(567) should NOT insert a second node without bidderName', done => {
     const bst = new BST()
     bst.add(119)
-    try{
+    try {
       bst.add(567)
-    } catch(err) {
+    } catch (err) {
       expect(err.message).to.be.equal('Reserve price must be set once!')
     }
     done()
   })
-  it('add(99) should insert a node on the left root in the binary search tree because 99 < 109', (done) => {
+  it('add(99) should insert a node on the left root in the binary search tree because 99 < 109', done => {
     const bst = new BST()
     bst.add(119)
     bst.add(99, 'alpha')
@@ -33,7 +33,7 @@ describe('Binary search tree', () => {
     expect(bst.root.right).to.be.equal(null)
     done()
   })
-  it('add(147) should insert a node on the right root in the binary search tree because 147 > 109', (done) => {
+  it('add(147) should insert a node on the right root in the binary search tree because 147 > 109', done => {
     const bst = new BST()
     bst.add(119)
     bst.add(147, 'beta')
@@ -42,7 +42,7 @@ describe('Binary search tree', () => {
     expect(bst.root.left).to.be.equal(null)
     done()
   })
-  it('search() should return the researched node if the key is found', (done) => {
+  it('search() should return the researched node if the key is found', done => {
     const bst = new BST()
     bst.add(27)
     bst.add(14, 'alfa')
@@ -56,7 +56,7 @@ describe('Binary search tree', () => {
     expect(foundNode.bidders).to.be.eql(['bravo'])
     done()
   })
-  it('search() should NOT return the researched node if the key is not found', (done) => {
+  it('search() should NOT return the researched node if the key is not found', done => {
     const bst = new BST()
     bst.add(27)
     bst.add(14, 'alfa')
@@ -68,16 +68,16 @@ describe('Binary search tree', () => {
     expect(bst.search(999)).to.be.equal(false)
     done()
   })
-  it('search() should NOT return the researched node if the bst is empty', (done) => {
+  it('search() should NOT return the researched node if the bst is empty', done => {
     try {
-     const bst = new BST()
-     bst.search(999)
-    }catch (err) {
+      const bst = new BST()
+      bst.search(999)
+    } catch (err) {
       expect(err.message).to.be.equal('BST is empty!')
     }
     done()
   })
-  it('getMax() should return the maximum node of the BST', (done) => {
+  it('getMax() should return the maximum node of the BST', done => {
     const bst = new BST()
     bst.add(27)
     bst.add(14, 'alfa')
@@ -89,7 +89,7 @@ describe('Binary search tree', () => {
     expect(bst.getMax()).to.be.equal(42)
     done()
   })
-  it('check if BST is a BST should return true(will be useful when rotations will be apply RB Tree or AVL Tree)', (done) => {
+  it('check if BST is a BST should return true(will be useful when rotations will be apply RB Tree or AVL Tree)', done => {
     const bst = new BST()
     bst.add(27)
     bst.add(14, 'alfa')
--- test/node-test.js
+++ test/node-test.js
@@ -1,59 +1,59 @@
-`use strict`
+;`use strict`
 
 const chai = require('chai')
 const expect = chai.expect
 const Node = require('../lib/node.js')
 
 describe('Node', () => {
-  it('constructor(12.5) should NOT create a node passing a float because, the key must be an integer.', (done) => {
+  it('constructor(12.5) should NOT create a node passing a float because, the key must be an integer.', done => {
     try {
       const node = new Node(12.5)
-    } catch(err) {
+    } catch (err) {
       expect(err.message).to.equal('must be an integer!')
     }
     done()
   })
-  it('constructor(true) should NOT create a node passing a boolean  because, the key must be an integer.', (done) => {
+  it('constructor(true) should NOT create a node passing a boolean  because, the key must be an integer.', done => {
     try {
       const node = new Node('coucou')
-    } catch(err) {
+    } catch (err) {
       expect(err.message).to.equal('must be an integer!')
     }
     done()
   })
-  it('constructor(undefined) should NOT create a node passing a string  because, the key must be an integer.', (done) => {
+  it('constructor(undefined) should NOT create a node passing a string  because, the key must be an integer.', done => {
     try {
       const node = new Node(undefined)
-    } catch(err) {
+    } catch (err) {
       expect(err.message).to.equal('must be an integer!')
     }
     done()
   })
-  it('constructor({}) should NOT create a node passing an object  because, the key must be an integer.', (done) => {
+  it('constructor({}) should NOT create a node passing an object  because, the key must be an integer.', done => {
     try {
       const node = new Node('coucou')
-    } catch(err) {
+    } catch (err) {
       expect(err.message).to.equal('must be an integer!')
     }
     done()
   })
-  it('constructor(null) should NOT create a node passing a null  because, the key must be an integer.', (done) => {
+  it('constructor(null) should NOT create a node passing a null  because, the key must be an integer.', done => {
     try {
       const node = new Node(null)
-    } catch(err) {
+    } catch (err) {
       expect(err.message).to.equal('must be an integer!')
     }
     done()
   })
-  it('constructor(`12`) should NOT create a node passing a string  because, the key must be an integer.', (done) => {
+  it('constructor(`12`) should NOT create a node passing a string  because, the key must be an integer.', done => {
     try {
       const node = new Node('12')
-    } catch(err) {
+    } catch (err) {
       expect(err.message).to.equal('must be an integer!')
     }
     done()
   })
-  it('constructor(667) should create a node with a bidders property having *reservePrice(empty) and a key equal to 667.', (done) => {
+  it('constructor(667) should create a node with a bidders property having *reservePrice(empty) and a key equal to 667.', done => {
     const node = new Node(667)
     expect(node.key).to.equal(667)
     expect(node.bidders).to.eql(['*reservePrice'])
@@ -61,7 +61,7 @@ describe('Node', () => {
     expect(node.right).to.equal(null)
     done()
   })
-  it('constructor(667, marcus) should create a node with a bidders property having marcus and a key equal to 667.', (done) => {
+  it('constructor(667, marcus) should create a node with a bidders property having marcus and a key equal to 667.', done => {
     const node = new Node(667, 'marcus')
     expect(node.key).to.equal(667)
     expect(node.bidders).to.eql(['marcus'])
@@ -69,7 +69,7 @@ describe('Node', () => {
     expect(node.right).to.equal(null)
     done()
   })
-  it('addBidder() should NOT insert a new bidder in the bidders property, because bidderName cannot be null.', (done) => {
+  it('addBidder() should NOT insert a new bidder in the bidders property, because bidderName cannot be null.', done => {
     try {
       const node = new Node(957, 'charly')
       node.addBidder()
@@ -78,58 +78,66 @@ describe('Node', () => {
     }
     done()
   })
-  it('addBidder(bravo) should insert a new bidder in the bidders property.', (done) => {
+  it('addBidder(bravo) should insert a new bidder in the bidders property.', done => {
     const node = new Node(957, 'charly')
     node.addBidder('bravo')
     expect(node.bidders).to.eql(['charly', 'bravo'])
     done()
   })
-  it('addLeftNode(node) should NOT insert on the left a new node having a key value greater than the current one.', (done) => {
+  it('addLeftNode(node) should NOT insert on the left a new node having a key value greater than the current one.', done => {
     const node1 = new Node(957, 'charly')
     const node2 = new Node(958, 'delta')
-   try {
-     node1.addLeftNode(node2)
+    try {
+      node1.addLeftNode(node2)
     } catch (err) {
-      expect(err.message).to.equal(`New node value must be lesser than the current one : ${node2.key} > ${node1.key}.`) 
-    } 
+      expect(err.message).to.equal(
+        `New node value must be lesser than the current one : ${node2.key} > ${
+          node1.key
+        }.`
+      )
+    }
     done()
   })
-  it('addLeftNode(node) should NOT insert on the left anything <> Node object.', (done) => {
+  it('addLeftNode(node) should NOT insert on the left anything <> Node object.', done => {
     const node1 = new Node(957, 'charly')
-   try {
-     node1.addLeftNode()
+    try {
+      node1.addLeftNode()
     } catch (err) {
-      expect(err.message).to.equal(`must be a Node object!`) 
-    } 
+      expect(err.message).to.equal(`must be a Node object!`)
+    }
     done()
   })
-  it('addLeftNode(node) should insert on the left a new node having a key value lesser than the current one.', (done) => {
+  it('addLeftNode(node) should insert on the left a new node having a key value lesser than the current one.', done => {
     const node1 = new Node(957, 'charly')
     const node2 = new Node(858, 'delta')
     node1.addLeftNode(node2)
     expect(node1.left.key).to.be.lt(node1.key)
     done()
   })
-  it('addRightNode(node) should NOT insert on the right a new node having a key value lesser than the current one.', (done) => {
+  it('addRightNode(node) should NOT insert on the right a new node having a key value lesser than the current one.', done => {
     const node1 = new Node(957, 'charly')
     const node2 = new Node(956, 'delta')
-   try {
-     node1.addRightNode(node2)
+    try {
+      node1.addRightNode(node2)
     } catch (err) {
-      expect(err.message).to.equal(`New node value must be greater than the current one : ${node2.key} < ${node1.key}.`) 
-    } 
+      expect(err.message).to.equal(
+        `New node value must be greater than the current one : ${node2.key} < ${
+          node1.key
+        }.`
+      )
+    }
     done()
   })
-  it('addRightNode(node) should NOT insert on the right anything <> Node object.', (done) => {
+  it('addRightNode(node) should NOT insert on the right anything <> Node object.', done => {
     const node1 = new Node(957, 'charly')
-   try {
-     node1.addRightNode()
+    try {
+      node1.addRightNode()
     } catch (err) {
-      expect(err.message).to.equal(`must be a Node object!`) 
-    } 
+      expect(err.message).to.equal(`must be a Node object!`)
+    }
     done()
   })
-  it('addRightNode(node) should insert on the right a new node having a key value greater than the current one.', (done) => {
+  it('addRightNode(node) should insert on the right a new node having a key value greater than the current one.', done => {
     const node1 = new Node(957, 'charly')
     const node2 = new Node(1008, 'delta')
     node1.addRightNode(node2)
